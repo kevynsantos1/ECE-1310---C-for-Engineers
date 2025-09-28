b2ddc8a8d28992f90fb25d5b8433275bbcdc5e35
kevynsantos1 <kevynsantos1804@gmail.com>
2025-09-28 15:19:16 -0700

    Add files via upload

---
+++ Learning If and Else/else_and_if.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+
+  int x;
+  int y;
+  int z;
+
+  cout<<"Hello, what is your age?"<<endl;
+  cin>>x;
+  
+  if(x<40){
+  int y=(x-18);
+        cout<<"Your age is less than 40 and you were 18 years old "<<y<<" years ago."<<endl;
+  }
+  
+  else{
+  int z=(x-40);
+        cout<<"Your age is more than 40 and you were 40 years old "<<z<<" years ago."<<endl;
+  }
+  
+  cout<<"Bye bye";
+  
+  
+  
+  
+  
+  
+  return 0;
+}
---
+++ Learning If and Else/if_no_else.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+
+  char x;
+
+
+  cout << "Pick a single character please" << endl;
+  cin >> x;
+  if (x == 'A')
+    cout << "Your grade is: A" << endl;
+
+  cout << "Good bye" << endl;
+
+  int y;
+
+  cout << "Pick an integer please" << endl;
+  cin >> y;
+  if ((y > 0) && (y < 10))
+    cout << "The number " << y << " is between 0 and 10" << endl;
+
+  cout<<"Good bye";
+  
+  
+  return 0;
+}
