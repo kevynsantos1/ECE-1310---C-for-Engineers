fdbfa3d281e163a2373b3fa99bc585b084a6bb88
kevynsantos1 <kevynsantos1804@gmail.com>
2025-09-28 15:36:52 -0700

    Add files via upload

---
+++ Learning If and Else/if_else_birth_months_assignment.cpp
@@ -0,0 +1,67 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int x;
+    
+    cout<<"What is your birth months number?"<<endl;
+    cin>>x;
+    if (x==1){
+        cout<<"Your birth month is January"<<endl;}
+        
+    else if (x==2){
+        cout<<"Your birth month is February"<<endl;}
+        
+    else if (x==3){
+        cout<<"Your birth month is March"<<endl;}
+    
+    else if (x==4){
+        cout<<"Your birth month is April"<<endl;}
+        
+    else if (x==5){
+        cout<<"Your birth month is May"<<endl;}
+        
+    else if (x==6){
+        cout<<"Your birth month is June"<<endl;}
+        
+    else if (x==7){
+        cout<<"Your birth month is July"<<endl;}
+        
+    else if (x==8){
+        cout<<"Your birth month is August"<<endl;}
+        
+    else if (x==9){
+        cout<<"Your birth month is September"<<endl;}
+        
+    else if (x==10){
+        cout<<"Your birth month is October"<<endl;}
+        
+    else if (x==11){
+        cout<<"Your birth month is November"<<endl;}
+        
+    else if (x==12){
+        cout<<"Your birth month is December"<<endl;}
+        
+    else
+        cout<<"you lyin";
+    
+        
+        
+        
+        
+        
+        
+        
+    return 0;
+}
